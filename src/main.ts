b65393f45ce69813149de744fd458aba477358f4
Kyra <kyra.dev.07@gmail.com>
2025-04-19 17:04:28 -0500

    Se agrega configuración de validador de pipe global para todos los controladores, que permiten verificar la data entrante que cumpla con los criterios definidos

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+
+    //This global configuration will be applied to all the controllers, without having to add it to each controller
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,//Only accepts the properties that are defined in the DTO, otherwise them will be deleted
+            forbidNonWhitelisted: true,//Will throw an error if the DTO has properties that are not defined in it
+        })
+    )
+
+
+    await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
