7d8e4d4a0f66412d6501f42cbfddbabb06090849
Kyra <kyra.dev.07@gmail.com>
2025-04-21 20:33:15 -0500

    Se exportan los servicios de cars y brands para llenar por medio del servicio del seed, la data correspondiente

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { BrandsService } from "../brands/brands.service";
+import { CarsService } from "../cars/cars.service";
+import { CARS_SEED } from "./data/cars.seed";
+import { BRANDS_SEED } from "./data/brands.seed";
 
 @Injectable()
 export class SeedService {
 
-    runSeed() {
+    @Inject()
+    private carsService: CarsService;
+
+    @Inject()
+    private brandsService: BrandsService;
+
+
+    runSeed(): string {
+        this.carsService.fillCarsSeed(CARS_SEED);
+        this.brandsService.fillBrandsSeed(BRANDS_SEED);
         return `This action returns all seed`;
     }
 
